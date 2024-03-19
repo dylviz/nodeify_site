2a1e2862bef551beacf18c24fa54516b1c4b0a3a
dylan <dylanvizcarra@gmail.com>
2024-03-19 09:19:23 -0700

    added text to about

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,7 @@ import {
   Heading,
   Icon,
   Stack,
+  Text,
 } from "@chakra-ui/react";
 import { SEO } from "components/seo/seo";
 import type { NextPage } from "next";
@@ -68,12 +69,18 @@ const Home: NextPage = () => {
           />
         </Box>
 
-        {/* <PricingSection /> */}
-
         {/* <FaqSection /> */}
-        <Box id="aboutSection" maxW={"4xl"} mx={"auto"} py={8}>
-          <Heading>About</Heading>
-          <p>This is the about section</p>
+        <Box id="aboutSection" maxW={"4xl"} mx={"auto"} py={12}>
+          <Heading textAlign={"center"}>About</Heading>
+          <Text p={4} fontSize={"xl"}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum
+          </Text>
         </Box>
 
         <ContactSection />
